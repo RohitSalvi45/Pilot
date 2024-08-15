0d01e9e6cfdab1675d4079b56b597c5c93b9cb26
Rohit Salvi <rohitsalvi003@gmail.com>
2024-08-15 13:30:28 -0400

    second commit

--- src/JavaBasics.java
+++ src/JavaBasics.java
@@ -1,5 +1,17 @@
 
 public class JavaBasics 
 {
-
+public static void main(String args[])
+{
+	int number = 3;
+	String word = "Rohit";
+	double number2 = 3.99;
+	boolean TorF = true;
+	
+	System.out.println("number is "+number+". String is "+word+
+			". And the other two are "+number2+", and "+TorF+".");	
+	
+	
+	
+}
 }
